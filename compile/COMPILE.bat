17a019731d41322b51e03d68ef9c7e9d28a30c51
kxde <kxdkxd@proton.me>
2025-04-16 02:01:11 -0300

    Delete compile directory

--- compile/COMPILE.bat
+++
@@ -1,7 +0,0 @@
-pyinstaller --onefile --windowed --icon="icon.ico" ^
---add-data "icon.png;." ^
---add-data "spine_viewer_settings.json;." ^
---add-data "BD2 Characters - Characters.csv;." ^
---add-data "%USERPROFILE%\\AppData\\Local\\Programs\\Python\\Python313\\Lib\\site-packages\\UnityPy\\resources;UnityPy/resources" ^
---additional-hooks-dir=. ^
-BD2pineViewer.pyw
